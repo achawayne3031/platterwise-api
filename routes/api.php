77e5fa327baa003c6182534b2bc592cbe9ed49fd
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2023-11-13 11:13:14 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+///// Paystack WebHOOK /////
+Route::group(
+    [
+        'namespace' => 'App\Http\Controllers',
+    ],
+    function ($router) {
+        Route::post(
+            '/paystack/webhook',
+            'PaystackWebhookController@handleWebhook'
+        );
+    }
+);
